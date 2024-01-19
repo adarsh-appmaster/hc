74cbcc7283832f78966f9d20ee632d65af42ad02
adarsh-appmaster <75722975+adarsh-appmaster@users.noreply.github.com>
2024-01-19 18:20:24 +0530

    03-tut

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    
+    
+    <h1 className='bg-green-400 text-black p-4 rounded xl'>Tailwind Test</h1>
+    </>
+  )
+}
+
+export default App
